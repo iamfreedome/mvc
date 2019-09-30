fa4c394c91cfccfae4704bad4873cd955e715f72
iamfreedome <54803685+iamfreedome@users.noreply.github.com>
2019-09-30 13:44:56 +0400

    Delete MY_controller.php

--- application/core/MY_controller.php
+++
@@ -1,9 +0,0 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-
-class MY_controller extends CI_Controller {
-	public function __construct() {
-		parent::__construct();
-	}
-}
-?>
