d1ac1987e6623a896e03a6042bcaaefc57783c37
iamfreedome <iamfreedome@ya.ru>
2019-10-01 13:23:31 +0400

    fix some markup into views

--- application/views/board/login.php
+++ application/views/board/login.php
@@ -1,10 +1,10 @@
 
   <?php $this->load->view('templates/header_board',array('title' => 'Страница Авторизации!')); ?>
-		<div class="col-lg-5 col-lg-offset-2">
-			<p>Заполните поля для авторизации на нашем сайте
-			</p>
+	<div class="col-lg-5 col-lg-offset-2">
+		<p>Заполните поля для авторизации на нашем сайте
+		</p>
 	
-	<?php echo validation_errors('<div class="alert alert-danger">','</div>'); ?>
+		<?php echo validation_errors('<div class="alert alert-danger">','</div>'); ?>
 		<form action="" method="POST"> 
 			<div class="form-group">
 				<label for="username" > Имя пользователя (USERNAME) </label>
@@ -21,5 +21,5 @@
 			</div> 
 		</form>
 
-		</div>
+	</div>
    <?php $this->load->view('templates/footer_board');	?>
--- application/views/board/posts.php
+++ application/views/board/posts.php
@@ -1,12 +1,12 @@
- <?php $this->load->view('templates/header_board',array('title' => "Доска пользователя $uname !"));?>
+<?php $this->load->view('templates/header_board',array('title' => "Доска пользователя $uname !"));?>
 
 <?php $this->load->view('templates/auth_header',$ai);?> 
 
 <!--POSTS HERE-->
- <?php $this->load->view('board/post_other', array('resi' => $resi, 'pi' => $pi, 'ai' => $ai)); ?>
+<?php $this->load->view('board/post_other', array('resi' => $resi, 'pi' => $pi, 'ai' => $ai)); ?>
 
 <?php $this->load->view('board/script');?>	
 	
- <?php $this->load->view('templates/footer_board');?>
+<?php $this->load->view('templates/footer_board');?>
  
  
